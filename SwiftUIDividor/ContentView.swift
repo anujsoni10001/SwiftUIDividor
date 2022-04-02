245b5b070333ddc648b00c862cbe72fdd51c3d71
Anuj Soni <anujsoni10001@gmail.com>
2022-04-02 07:52:48 +0530

    Initial Commit

---
+++ SwiftUIDividor/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftUIDividor
+//
+//  Created by Anuj Soni on 02/04/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftUIDividor/SwiftUIDividorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIDividorApp.swift
+//  SwiftUIDividor
+//
+//  Created by Anuj Soni on 02/04/22.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIDividorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
