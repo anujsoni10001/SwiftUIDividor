8bba2d65f98eb4001a2f3f9deaca24a4eff61dd9
Anuj Soni <anujsoni10001@gmail.com>
2022-04-02 07:58:07 +0530

    SwiftUI Divider Tutorial

--- SwiftUIDividor/ContentView.swift
+++ SwiftUIDividor/ContentView.swift
@@ -9,8 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack(spacing:20){
+                    Group {
+                        // 1.
+                        Text("Horizontal Divider")
+                        Divider()
+                        Image(systemName: "1.circle")
+                        Divider()
+                    }
+                    
+                    Group {
+                        Text("Vertical Divider")
+                        // 2.
+                        HStack {
+                            Divider()
+                            Image(systemName: "2.circle")
+                            Divider()
+                        }.frame(height: 100)
+                        
+                        Group {
+                            // 3.
+                            Text("Change Divider Color")
+                            Image(systemName: "3.circle")
+                            Divider().background(Color.red)
+                            
+                            // 4.
+                            Text("Change Divider Size")
+                            Image(systemName: "4.circle")
+                            Divider().frame(width: 200)
+                        }
+                    }
+                }
     }
 }
 
